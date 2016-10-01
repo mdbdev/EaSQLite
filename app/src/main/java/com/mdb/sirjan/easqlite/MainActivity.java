18c23bfa833f2863b9d8475e8b9f227ff0632a13
codyjhsieh <codyjhsieh@gmail.com>
2016-10-01 11:41:03 -0700

    test to level

--- app/src/main/java/com/mdb/sirjan/easqlite/MainActivity.java
+++ app/src/main/java/com/mdb/sirjan/easqlite/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Ayyyy", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
