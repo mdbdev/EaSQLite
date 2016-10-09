43e2e097cdc4e14aa2f02661c6895d1c733ac11d
Andy Wang <andywcal@berkeley.edu>
2016-10-09 00:40:12 -0700

    added new exception type and created the Entry class

---
+++ easqlitelib/src/main/java/com/mdb/easqlitelib/exceptions/InvalidTypeException.java
@@ -0,0 +1,13 @@
+package com.mdb.easqlitelib.exceptions;
+
+/**
+ * Created by andyw1997 on 10/9/16.
+ * Invalid Type Exception thrown when adding
+ * row values in that are not valid
+ */
+
+public class InvalidTypeException extends Exception {
+    public InvalidTypeException() {
+        super();
+    }
+}
--- easqlitelib/src/main/java/com/mdb/easqlitelib/structures/Entry.java
+++ easqlitelib/src/main/java/com/mdb/easqlitelib/structures/Entry.java
@@ -1,9 +1,58 @@
 package com.mdb.easqlitelib.structures;
 
+import com.mdb.easqlitelib.exceptions.InvalidTypeException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Abstraction of a Row in a SQLite Database
  */
 
 public class Entry {
     public int id;
+    public List<Object> data;
+    public Table table;
+
+    public Entry(int id, List<Object> list, Table table) {
+        this.id = id;
+        this.table = table;
+        data = new ArrayList<Object>(list);
+    }
+
+    public String classType(Object o) {
+        if (o == null) {
+            return "NULL";
+        }
+        Class c = o.getClass();
+        if (c==Integer.class) {
+            return "INTEGER";
+        } else if (c==Float.class||c==Double.class) {
+            return "REAL";
+        } else if (c==char[].class||c==String.class) {
+            return "TEXT";
+        } else {
+            return null;
+        }
+    }
+
+    public void addField(Object field) throws InvalidTypeException{
+        String type = classType(field);
+        if (type != null && !type.equals(table.getSchema().get(data.size()))) {
+            throw new InvalidTypeException();
+        } else {
+            data.add(field);
+        }
+    }
+
+    public boolean verifyFields(){
+        for (int i = 0; i < data.size(); i++) {
+            String type = classType(data.get(i));
+            if (type == null || !type.equals(table.getSchema().get(i))) {
+                return false
+            }
+        }
+        return true;
+    }
+
 }
