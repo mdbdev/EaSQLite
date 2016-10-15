ea9e9c3d58b5781f3d1e53360a7e686b6518ebdb
Sirjan Kafle <sxk1693@berkeley.edu>
2016-10-15 11:28:42 -0700

    Add dbHandler instance variable in TableCommands

--- easqlitelib/src/main/java/com/mdb/easqlitelib/commands/TableCommands.java
+++ easqlitelib/src/main/java/com/mdb/easqlitelib/commands/TableCommands.java
@@ -1,7 +1,10 @@
 package com.mdb.easqlitelib.commands;
 
+import com.mdb.easqlitelib.DatabaseHandler;
+
 /**
  * Created by sirjan on 10/15/16.
  */
 public class TableCommands {
+    private DatabaseHandler dbHandler;
 }
