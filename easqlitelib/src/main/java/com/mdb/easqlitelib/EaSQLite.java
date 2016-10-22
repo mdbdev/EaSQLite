08f1bd3a8dc5c0c16ab4e5ec436f0edb589d16b0
codyjhsieh <codyjhsieh@gmail.com>
2016-10-22 11:44:28 -0700

    add createTable with Pairs

--- easqlitelib/src/main/java/com/mdb/easqlitelib/EaSQLite.java
+++ easqlitelib/src/main/java/com/mdb/easqlitelib/EaSQLite.java
@@ -1,4 +1,5 @@
 package com.mdb.easqlitelib;
+
 import android.content.Context;
 import android.util.Pair;
 
@@ -14,6 +15,7 @@ public class EaSQLite {
     /**
      * Initialize EaSQLite by initializing our Database Handler with a specified
      * context from the Application.
+     *
      * @param context the context provided by the Application.
      */
     public static void initialize(Context context) {
@@ -26,8 +28,16 @@ public class EaSQLite {
     }
 
     // Create a table given a tableName and columnNames
-    public static boolean createTable(String tableName, String[] columnNames) {
-        return false;
+    public static boolean createTable(String tableName, Pair<String, String>[] columnList) {
+        boolean working = dbHandler.createTable(tableName);
+        for (Pair<String, String> p : columnList) {
+            try {
+                working &= dbHandler.addColumn(tableName, p.first, p.second);
+            } catch (Exception e) {
+                System.out.print(e);
+            }
+        }
+        return working;
     }
 
     // Delete a table given the tableName
@@ -61,12 +71,12 @@ public class EaSQLite {
     }
 
     //Add single column
-    public static boolean addColumn(String tableName, String columnName, String type) throws InvalidTypeException{
+    public static boolean addColumn(String tableName, String columnName, String type) throws InvalidTypeException {
         return dbHandler.addColumn(tableName, columnName, type);
     }
 
     //Add multiple columns with an array of names
-    public static boolean addColumns(String tableName, Pair<String, String>[] columns) throws InvalidTypeException{
+    public static boolean addColumns(String tableName, Pair<String, String>[] columns) throws InvalidTypeException {
         return dbHandler.addColumns(tableName, columns);
     }
 
@@ -81,37 +91,37 @@ public class EaSQLite {
     }
 
     //Add entry to SQLite database
-    public static boolean addRow(String tableName, Pair<String, String>[] entries) throws InvalidTypeException{
+    public static boolean addRow(String tableName, Pair<String, String>[] entries) throws InvalidTypeException {
         return dbHandler.addRow(tableName, entries);
     }
 
     //Delete entry from DB
-    public static boolean deleteRow(String tableName, int id){
+    public static boolean deleteRow(String tableName, int id) {
         return dbHandler.deleteRow(tableName, id);
     }
 
     //Delete first entry from DB
-    public static boolean deleteFirstRow(String tableName){
+    public static boolean deleteFirstRow(String tableName) {
         return true;
     }
 
     //Delete last entry from DB
-    public static boolean deleteLastRow(String tableName){
+    public static boolean deleteLastRow(String tableName) {
         return true;
     }
 
     //Delete all entries from DB
-    public static boolean deleteAllRows(String tableName){
+    public static boolean deleteAllRows(String tableName) {
         return true;
     }
 
     //Get entry by entry id returned by create row
-    public static List<Object> getRowById(String tableName, int id){
+    public static List<Object> getRowById(String tableName, int id) {
         return dbHandler.getRowById(tableName, id);
     }
 
     //Create table to store custom objects
-    public static String[] createTableFromObject(Object obj){
+    public static String[] createTableFromObject(Object obj) {
         return null;
     }
 
