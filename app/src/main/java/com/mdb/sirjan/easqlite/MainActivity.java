474c7521cb368ed2f468c094bfd9d29199974bb9
Sirjan Kafle <sxk1693@berkeley.edu>
2016-10-29 13:31:42 -0700

    Create tab layout initial

--- app/src/main/java/com/mdb/sirjan/easqlite/MainActivity.java
+++ app/src/main/java/com/mdb/sirjan/easqlite/MainActivity.java
@@ -1,81 +1,148 @@
 package com.mdb.sirjan.easqlite;
 
-import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.view.ViewGroup;
+
 import android.widget.TextView;
-
-import com.mdb.easqlitelib.*;
-import com.mdb.easqlitelib.exceptions.InvalidTypeException;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textViewColumn;
-    private TextView textViewRow;
-    private EditText editTextColumn;
-    private EditText editTextRow;
-    private Button addColumn;
-    private Button addRow;
-    private List<String> columnList;
-    private List<String> rowList;
-    private String TABLE_NAME = "testTable";
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    private ViewPager mViewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        columnList = new ArrayList<String>();
-        EaSQLite.initialize(getApplicationContext());
-        EaSQLite.createTable(TABLE_NAME);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        textViewColumn = (TextView) findViewById(R.id.textViewColumn);
-        textViewRow = (TextView) findViewById(R.id.textViewRow);
-        editTextColumn = (EditText) findViewById(R.id.editTextColumn);
-        editTextRow = (EditText) findViewById(R.id.editTextRow);
-        addColumn = (Button) findViewById(R.id.buttonColumn);
-        addRow = (Button) findViewById(R.id.buttonRow);
-        refreshStuff();
-        addColumn.setOnClickListener(new View.OnClickListener() {
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                try{
-                EaSQLite.addColumn(TABLE_NAME, editTextColumn.getText().toString(), "String");
-                refreshStuff();}
-                catch (InvalidTypeException e){
-                    System.out.println(e);
-                }
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
-        addRow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                rowList.add(editTextRow.getText().toString());
-                textViewRow.setText(textViewRow.getText().toString() + ", " + editTextRow.getText().toString());
-
-            }
-        });
-
 
     }
-    private void refreshStuff(){
-        textViewColumn.setText("Column Names:");
-        columnList = new ArrayList<String>();
-        for(String s : EaSQLite.getColumnNames(TABLE_NAME)){
 
-            columnList.add(s);
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
-        for(String s : columnList){
 
-            textViewColumn.setText(textViewColumn.getText().toString() + " " + s);
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
         }
 
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "SECTION 1";
+                case 1:
+                    return "SECTION 2";
+                case 2:
+                    return "SECTION 3";
+            }
+            return null;
+        }
     }
 }
